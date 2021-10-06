c16bf35e134e2f06007f4e8f53889b6a9c9a4352
cynthia <yinnycfu@gmail.com>
2021-10-06 18:31:23 +0100

    feat: add various responses

--- src/main/java/MagicEightBall.java
+++ src/main/java/MagicEightBall.java
@@ -9,7 +9,24 @@ public class MagicEightBall {
         String userInput = scanner.next();
         scanner.close();
 
-        System.out.println("Cool Story, Bro.");
+        int randomNumber = 1;
 
+        if (randomNumber == 0) {
+            System.out.println("Cool Story, Bro.");
+        } else if (randomNumber == 1) {
+            System.out.println("I guess so.");
+        } else if (randomNumber == 2) {
+            System.out.println("Yassssss.");
+        } else if (randomNumber == 3) {
+            System.out.println("Sounds alright.");
+        } else if (randomNumber == 4) {
+            System.out.println("Nah, better not.");
+        } else if (randomNumber == 5) {
+            System.out.println("Just do it.");
+        } else if (randomNumber == 6) {
+            System.out.println("Yes.");
+        } else {
+            System.out.println("Don't do it!");
+        }
     }
 }
