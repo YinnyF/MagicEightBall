beb1c9107c8dd92c84b7f53c06749032265c3752
cynthia <yinnycfu@gmail.com>
2021-10-06 22:45:52 +0100

    feat: click counter

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,21 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
 
     public GUI() {
         // the window
-        JFrame frame = new JFrame();
+        frame = new JFrame();
 
         JButton button = new JButton("Click me");
+        button.addActionListener(this);
+
+        label = new JLabel("Number of clicks: 0");
+
         // makes the panel
-        JPanel panel = new JPanel();
+        panel = new JPanel();
 
         // a border object
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         // a layout manager object
         panel.setLayout(new GridLayout(0, 1));
         panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -29,5 +40,12 @@ public class GUI {
         new GUI();
 
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // action to perform when button is clicked
+        count++;
+        label.setText("Number of clicks: " + count);
     }
 }
