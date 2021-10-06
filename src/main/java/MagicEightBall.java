d7b1f16d6f57776e73ecc8c2773028bf9b8c433a
cynthia <yinnycfu@gmail.com>
2021-10-06 18:15:55 +0100

    feat: user input and fixed response

--- src/main/java/MagicEightBall.java
+++ src/main/java/MagicEightBall.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class MagicEightBall {
     public static void main(String[] args) {
+        System.out.println("Ask me anything.");
+
+        Scanner scanner = new Scanner(System.in);
+        // not necessary to store user input
+        String userInput = scanner.next();
+        scanner.close();
+
+        System.out.println("Cool Story, Bro.");
 
     }
 }
